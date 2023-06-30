990609c7787d996fc173c1c0dc1cff99fc1b40bd
Jordan Rollins <carpeomniavino@gmail.com>
2023-06-30 23:25:17 +0200

    WIP: ItemNav

---
+++ app/components/item-nav.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class ItemNavComponent extends Component {
+ 
+  options = [
+    { label: 'Groceries', route: 'products' },
+    { label: 'Wine', route: 'wine' },
+    { label: 'Clothes', route: 'clothes' },
+    { label: 'FX', route: 'fx' },
+  ];
+
+ @tracked selectedOption = this.options[0];
+
+ @action
+ isSelected(option) {
+    return option.label === this.selectedOption.label;
+  }
+
+  @action
+  selectOption(option) {
+    this.selectedOption = option;
+  }
+}
