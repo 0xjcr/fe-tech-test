6f8c380c50bdbc42e8f46bf8e84592133ec7a9bc
Jordan Rollins <carpeomniavino@gmail.com>
2023-07-06 02:00:56 +0200

    WIP: Shopping cart update

--- app/routes/shopping-cart.js
+++ app/routes/shopping-cart.js
@@ -1,16 +1,22 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-
+import { tracked } from '@glimmer/tracking';
 
 export default class ShoppingCartRoute extends Route {
   @service cart;
+  @service cart;
+  @tracked quantity;
+  @tracked totalPayable;
+
+  constructor() {
+    super(...arguments);
+    this.quantity = this.cart.totalItems;
+    this.totalPayable = this.cart.totalAmount;
+  }
 
   model() {
     return {
-      quantity: this.cart.quantity,
-      products: this.cart.items.map((item) => item.product),
+      cart: this.cart,
     };
   }
-
-  
 }
